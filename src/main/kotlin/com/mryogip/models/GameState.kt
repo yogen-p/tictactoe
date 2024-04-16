ca45ecffe8d2596e5b844f056c3c6e4f5f509fa0
Yogen P <yogenparekh569@gmail.com>
2024-04-16 23:10:31 +0100

    Add models

---
+++ src/main/kotlin/com/mryogip/models/GameState.kt
@@ -0,0 +1,46 @@
+package com.mryogip.models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class GameState(
+    val playerInTurn: Char? = 'X',
+    val winningPlayer: Char? = null,
+    val isBoardFull: Boolean = false,
+    val field: Array<Array<Char?>> = emptyField(),
+    val connectedPlayers: List<Char> = emptyList(),
+) {
+    companion object {
+        fun emptyField(): Array<Array<Char?>> {
+            return arrayOf(
+                arrayOf(null, null, null),
+                arrayOf(null, null, null),
+                arrayOf(null, null, null),
+            )
+        }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as GameState
+
+        if (playerInTurn != other.playerInTurn) return false
+        if (!field.contentDeepEquals(other.field)) return false
+        if (winningPlayer != other.winningPlayer) return false
+        if (isBoardFull != other.isBoardFull) return false
+        if (connectedPlayers != other.connectedPlayers) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = playerInTurn?.hashCode() ?: 0
+        result = 31 * result + field.contentDeepHashCode()
+        result = 31 * result + (winningPlayer?.hashCode() ?: 0)
+        result = 31 * result + isBoardFull.hashCode()
+        result = 31 * result + connectedPlayers.hashCode()
+        return result
+    }
+}
